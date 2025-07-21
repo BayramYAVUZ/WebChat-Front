bd8e148f8f7c2c2e16a856a8b35c3305414d6283
BayramYAVUZ <bayramyvz1232003@gmail.com>
2025-07-21 20:13:30 +0300

    Secret temizlendi, güncel frontend

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 function App() {
   const directLine = useMemo(() => {
     return window.WebChat.createDirectLine({
-      token: "4Dii0jSaEzqbXAXwYyYXk6IKWljb3SE5U5n5YeH7oJ432ZY1IPjwJQQJ99BGAC5RqLJAArohAAABAZBS1gBj.G4GF3r6Ualt6l2pTl9SnxNdrFPHrxNSHYwenyv5k8rUlhhZusOHtJQQJ99BGAC5T7U2AArohAAABAZBS1B1i"
+     token: process.env.REACT_APP_BOT_SECRET
     });
   }, []);
 
